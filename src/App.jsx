904ac4a44f397d0a86b6b89f610f35d0ef626ff8
syedanasquadri <syedanasquadri123@gmail.com>
2025-09-28 10:37:34 +0530

    re-writng the App.jsx completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,5 @@
-import { useState } from "react"
 const App = () => {
-  const [InputVal, setInputVal] = useState("");
-  const [name,setname] = useState("");
-  const handleChange = (e) => {
-    setInputVal(e.target.value)
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setname(InputVal)
-  }
-
-  console.log(InputVal);
-  return <div>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="">Name</label><br />
-      <input type="text" onChange={handleChange}/><br />
-      <button >submit</button>
-    </form>
-    <ul>{name}</ul>
-  </div>
+    return
 }
+
 export default App
