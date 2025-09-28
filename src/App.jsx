3b62e066fc8059cdd44175d3fc4459d566c9d518
syedanasquadri <syedanasquadri123@gmail.com>
2025-09-28 11:52:03 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react"
+
 const App = () => {
-    return
+    const [form, setform] = useState({name:"", age:"" ,course:""});
+    const [leads, setleads] = useState([]);
+    const handleChange = (e) =>{
+        setform({...form, [e.target.name] : e.target.value});
+        
+    } 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setleads([...leads,form]);
+        setform({name: "", age: "", course: ""});
+    }
+    return <div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="">Name</label><br />
+            <input type="text" name="name" onChange={handleChange} value={form.name}/><br />
+            <label htmlFor="">Age</label><br />
+            <input type="text" name="age" onChange={handleChange}
+            value={form.age}/><br />
+            <label htmlFor="">Course</label><br />
+            <input type="text" name="course" onChange={handleChange}
+            value={form.course}/><br />
+            <button type="submit">Submit</button>
+            <h3>Leads</h3>            
+        </form>
+         <ul>
+         {/* {leads.map((leads,index) => <li key={index}>{leads.name} - {leads.age} - {leads.course}</li>)} */}
+         {leads.map((_,index) => <li key={index}>{_}</li>)}
+        </ul>
+    </div>
 }
 
 export default App
