a45a86ec75b34825302a80a0a3919c0eff0ec7ee
syedanasquadri <syedanasquadri123@gmail.com>
2025-10-01 15:58:26 +0530

    added a db.json file and removed localStorage functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,70 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
-    const [form, setform] = useState({name: "", age: "" , course: "", level: ""});
-    const [leads, setleads] = useState([]);
+  const [form, setForm] = useState({ name: "", age: "", course: "", level: "" });
+  const [leads, setLeads] = useState([]);
 
-
+  // Load leads from local "db.json" via json-server
   useEffect(() => {
-    localStorage.setItem("leads",JSON.stringify(leads))
-  },[leads])
+    fetch("http://localhost:5000/leads")//gives or returns http response of the string it has
+      .then((res) => res.json())//res.json converts that string into a js object, and returns that js object 
+      .then((data) => setLeads(data)); //data contains the js object, leads is updated with the data
+  }, []);
 
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
-    const handleChange = (e) =>{
-        setform({...form, [e.target.name] : e.target.value});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!form.name || !form.age || !form.course || !form.level){
+        alert("Fill all the fields.")
+        return;
     }
-     
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(!form.name || !form.age || !form.course || !form.level){
-            alert("Fill all the fields")
-            return;
-        }
-        setleads([...leads,form]);
-        setform({name: "", age: "", course: "" ,level: ""});
-    }
+    //saving data in db.json
+    fetch("http://localhost:5000/leads", {
+        method: "Post",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(form),
+    })
+    .then((res) => res.json())
+    .then((newLeads) => setLeads([...leads, newLeads]))
 
-    return <div>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="">Name</label><br />
-            <input type="text" name="name" onChange={handleChange} value={form.name}/><br />
+    setForm({name: "", age: "", course: "", level: ""})
+  }
 
-            <label htmlFor="">Age</label><br />
-            <input type="text" name="age" onChange={handleChange}
-            value={form.age}/><br />
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Name</label><br />
+        <input type="text" name="name" onChange={handleChange} value={form.name} /><br />
 
-            <label htmlFor="">Course</label><br />
-            <input type="text" name="course" onChange={handleChange}
-            value={form.course}/><br />
+        <label>Age</label><br />
+        <input type="text" name="age" onChange={handleChange} value={form.age} /><br />
 
-            <label htmlFor="">
-               <select name="level" onChange={handleChange} value={form.level}>
-                <option value="">--Select--</option>
-                 <option value="Beginner">Beginner</option>
-                 <option value="Pro">Pro</option>
-               </select>
-            </label>
+        <label>Course</label><br />
+        <input type="text" name="course" onChange={handleChange} value={form.course} /><br />
 
-            <button type="submit">Submit</button>
-            <h3>Leads</h3>            
-        </form>
-         <ul>
-         {leads.map((lead,index) => <li key={index}>{lead.name} - {lead.age} - {lead.course} - {lead.level} </li>)}
-        </ul>
+        <label>Level</label><br />
+        <select name="level" onChange={handleChange} value={form.level}>
+          <option value="">-- Select --</option>
+          <option value="Beginner">Beginner</option>
+          <option value="Pro">Pro</option>
+        </select><br /><br />
+
+        <button type="submit">Submit</button>
+        <h3>Leads</h3>
+      </form>
+
+      <ul>
+        {leads.map((lead) => (
+          <li key={lead.id}>
+            {lead.name} - {lead.age} - {lead.course} - {lead.level}
+          </li>
+        ))}
+      </ul>
     </div>
-}
+  );
+};
 
-export default App
+export default App;
